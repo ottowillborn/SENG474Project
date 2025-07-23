da737322e7ad555491fa73d0dbb2c483daf09153
tdgao <mr.trumgao@gmail.com>
2025-07-22 20:21:36 -0700

    remove test error line

--- learning_curves.py
+++ learning_curves.py
@@ -7,7 +7,6 @@ def plot_learning_curve(
     train_sizes: Union[List[int], np.ndarray],
     train_errors: Union[List[float], np.ndarray],
     val_errors: Union[List[float], np.ndarray],
-    test_errors: Union[List[float], np.ndarray] = None
 ) -> plt.Figure:
     """
     Plot a learning curve from pre-computed errors.
@@ -20,8 +19,6 @@ def plot_learning_curve(
         List or array of training errors corresponding to each training size
     val_errors : array-like
         List or array of validation errors corresponding to each training size
-    test_errors : array-like, optional
-        List or array of test errors corresponding to each training size
 
     Returns:
     --------
@@ -53,12 +50,6 @@ def plot_learning_curve(
     plt.plot(train_sizes, val_errors,
              label='Validation Error', color='green', marker='o')
 
-    # Plot test errors if provided
-    if test_errors is not None:
-        test_errors = np.array(test_errors)
-        plt.plot(train_sizes, test_errors,
-                 label='Test Error', color='red', marker='o')
-
     plt.grid(True)
     plt.legend(loc='best')
 
@@ -73,8 +64,7 @@ import numpy as np
 train_sizes = [100, 200, 300, 400, 500]
 train_errors = [0.5, 0.3, 0.2, 0.15, 0.1]
 val_errors = [0.6, 0.4, 0.35, 0.33, 0.32]
-test_errors = [0.55, 0.42, 0.38, 0.35, 0.34]
 
-fig = plot_learning_curve(train_sizes, train_errors, val_errors, test_errors)
+fig = plot_learning_curve(train_sizes, train_errors, val_errors)
 plt.show()
 """
