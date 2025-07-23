e531ffc4a1b3373bf6d2b9fa86288e0ad3b1b943
Kale Kasdorf <kale.kasdorf@gmail.com>
2025-07-23 14:56:52 -0700

    Outputs avg error and std for all folds

--- error_analysis.py
+++ error_analysis.py
@@ -1,7 +1,11 @@
 import subprocess
 import pandas as pd
+import numpy as np
 
-# Comment out dataframe print in model.py before running script
+# ***************
+# To use script: need output of model to be "{string}: error value int" (Splits by ": ")
+# Also change model name in line 18
+# ************** 
 
 years = list(range(2006, 2023))
 results = []
@@ -10,6 +14,7 @@ results = []
 for year in years:
     print(f"Running for {year}")
     result = subprocess.run(
+        # Change name of model below to run for different models
         ["python", "normal_equation.py", f"all_players_career_stats_{year}.csv"],
         stdout = subprocess.PIPE,
         stderr = subprocess.STDOUT,
@@ -26,8 +31,16 @@ for item in results:
     errors.append(value)
 
 #print errors
-print(errors)
+# print(errors)
 
 #make csv error file, for easy formatting to a spreadsheet
 df = pd.DataFrame({"Avg Error": errors})
 df.to_csv("errors.xlsx", index=False)
+
+# Error metrics for total LOOCV
+avg_error = np.mean(errors)
+#can't remember if we want std or variance, I think std?
+std_error = np.std(errors)
+variance_error = np.var(errors)
+
+print(f"Overall Error: {avg_error} +- {std_error}%")
